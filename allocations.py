6280e69287cf0607f7fcb281f8188d4903d56797
alake16 <aclake@umich.edu>
2020-02-21 12:56:00 -0600

    Implemented method for reading portfolio allocations

---
+++ allocations.py
@@ -0,0 +1,16 @@
+''' allocations.py
+This module handles the inputs for the tickers and respective allocations for the tracker to follow
+and analyze.
+
+TODO:
+	* write method for consuming ticker and allocation input from csv
+		* parse that data into appropriate data structure to be used by data and other modules
+
+'''
+import pandas as pd
+import os
+
+def read_allocations_input():
+	path = os.getcwd() + "/input/allocations.csv"
+	df = pd.read_csv(path)
+	return df
